0289da2492cedc150dcb43ac88a63407393aff17
Domingo Galdos <galdosd@gmail.com>
2018-06-07 19:30:18 -0400

    initial class the jython scripts will implement to receive events

---
+++ src/main/java/com/github/galdosd/betamax/ScriptWorld.java
@@ -0,0 +1,15 @@
+package com.github.galdosd.betamax;
+
+import com.github.galdosd.betamax.sprite.Sprite;
+import com.github.galdosd.betamax.sprite.SpriteTemplate;
+
+/**
+ * FIXME: Document this class
+ */
+public class ScriptWorld {
+    void onSpriteCreate(Sprite sprite) {}
+    void onSriteDestroy(Sprite sprite) {}
+    void onSpriteFrame(Sprite sprite) {}
+    void onSpriteClick(Sprite sprite) {}
+    void onBegin() {}
+}
