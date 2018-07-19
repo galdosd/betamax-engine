546f2dc660d1b67275851a7e7dda1faf45fd484c
Domingo Galdos <galdosd@gmail.com>
2018-07-19 16:31:54 -0400

    make dev console window wider, test repetitions

--- src/main/java/com/github/galdosd/betamax/gui/FxWindow.java
+++ src/main/java/com/github/galdosd/betamax/gui/FxWindow.java
@@ -82,7 +82,7 @@ public final class FxWindow extends Application {
         Scene scene = new Scene(sceneRoot);
         stage.setScene(scene);
         stage.setTitle("Betamax Developer Console");
-        stage.setWidth(640);
+        stage.setWidth(800);
         stage.setHeight(640);
         stage.show();
         stage.setOnCloseRequest( x -> { x.consume(); LOG.info("Closing developer console forbidden"); } );
