abfaae9f50514e5875118b847c04ccb16a6b627f
Domingo Galdos <galdosd@gmail.com>
2018-07-19 23:10:13 -0400

    gradually converge SoundExperiment to SoundRegsitry till i figure out this fucking bullshit

--- src/main/java/com/github/galdosd/betamax/sound/SoundExperiment.java
+++ src/main/java/com/github/galdosd/betamax/sound/SoundExperiment.java
@@ -1,68 +1,46 @@
 package com.github.galdosd.betamax.sound;
 
-import org.lwjgl.openal.AL;
-import org.lwjgl.openal.ALC;
-import org.lwjgl.openal.ALCCapabilities;
-import org.lwjgl.openal.ALCapabilities;
-import org.lwjgl.system.MemoryStack;
+import com.github.galdosd.betamax.OurTool;
+import lombok.Getter;
+import org.lwjgl.openal.*;
+import org.lwjgl.system.MemoryUtil;
 
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 
+import static com.google.common.base.Preconditions.checkState;
 import static org.lwjgl.openal.AL10.*;
 import static org.lwjgl.openal.ALC10.*;
 import static org.lwjgl.stb.STBVorbis.stb_vorbis_decode_filename;
-import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.stb.STBVorbis.stb_vorbis_decode_memory;
 import static org.lwjgl.system.libc.LibCStdlib.free;
 
 /**
  * FIXME: Document this class
  */
 public class SoundExperiment {
+    private static final IntBuffer channelsBuffer = MemoryUtil.memAllocInt(1);
+    private static final IntBuffer sampleRateBuffer = MemoryUtil.memAllocInt(1);
+    private static int channelsToFormat(int channels) {
+        if(channels == 1) return AL10.AL_FORMAT_MONO16;
+        if(channels == 2) return AL10.AL_FORMAT_STEREO16;
+        return -1;
+    }
+    long   device;
+    long  context;
     public static void main(String[] args) {
+        new SoundExperiment().doStuff();
+    }
 
-//Initialization
-        String defaultDeviceName = alcGetString(0, ALC_DEFAULT_DEVICE_SPECIFIER);
-        long   device            = alcOpenDevice(defaultDeviceName);
+    private void doStuff() {
+        initOpenAl();
 
-        int[] attributes = {0};
-        long  context    = alcCreateContext(device, attributes);
-        alcMakeContextCurrent(context);
-
-        ALCCapabilities alcCapabilities = ALC.createCapabilities(device);
-        ALCapabilities alCapabilities  = AL.createCapabilities(alcCapabilities);
-
-        ShortBuffer rawAudioBuffer;
-
-        int channels;
-        int sampleRate;
-
-        try (MemoryStack stack = stackPush()) {
-            //Allocate space to store return information from the function
-            IntBuffer channelsBuffer   = stack.mallocInt(1);
-            IntBuffer sampleRateBuffer = stack.mallocInt(1);
-
-            rawAudioBuffer = stb_vorbis_decode_filename("/tmp/test1.ogg", channelsBuffer, sampleRateBuffer);
-            System.out.println("Size " + rawAudioBuffer.limit() * Short.BYTES);
-
-            //Retreive the extra information that was stored in the buffers by the function
-            channels = channelsBuffer.get(0);
-            sampleRate = sampleRateBuffer.get(0);
-        }
-
-//Find the correct OpenAL format
-        int format = -1;
-        if (channels == 1) {
-            format = AL_FORMAT_MONO16;
-        } else if (channels == 2) {
-            format = AL_FORMAT_STEREO16;
-        }
-
-        int bufferPointer = alGenBuffers();
-        alBufferData(bufferPointer, format, rawAudioBuffer, sampleRate);
+        String filename = "test1.ogg";
+        SoundBuffer soundBufferHandle = loadSoundBuffer(filename);
         int sourcePointer = alGenSources();
 
-        alSourcei(sourcePointer, AL_BUFFER, bufferPointer);
+        alSourcei(sourcePointer, AL_BUFFER, soundBufferHandle.handle);
 
         alSourcePlay(sourcePointer);
         try {
@@ -72,9 +50,57 @@ public class SoundExperiment {
         }
 
         alDeleteSources(sourcePointer);
-        alDeleteBuffers(bufferPointer);
+
         alcDestroyContext(context);
         alcCloseDevice(device);
+    }
+
+    void checkAlError(){
+        int alError = alGetError();
+        checkState(alError==AL_NO_ERROR, "OpenAL error " + alError);
+        int alcError = alcGetError(device);
+        checkState(alcError==ALC_NO_ERROR, "OpenALC error " + alcError);
+    }
+
+    private void initOpenAl() {
+        String defaultDeviceName = alcGetString(0, ALC_DEFAULT_DEVICE_SPECIFIER);
+        device = alcOpenDevice(defaultDeviceName);
+        int[] attributes = {0};
+        context = alcCreateContext(device, attributes);
+        alcMakeContextCurrent(context);
+
+        ALCCapabilities alcCapabilities = ALC.createCapabilities(device);
+        ALCapabilities alCapabilities  = AL.createCapabilities(alcCapabilities);
+        checkAlError();
+    }
+
+    public static class SoundBuffer implements AutoCloseable{
+        @Getter int handle;
+        SoundBuffer(int handle) {
+            this.handle = handle;
+        }
+
+        @Override
+        public void close() throws Exception {
+            alDeleteBuffers(handle);
+        }
+    }
+
+    private static SoundBuffer loadSoundBuffer(String filename) {
+        ShortBuffer rawAudioBuffer;
+
+
+        ByteBuffer soundFileBuffer = OurTool.readOffHeapBuffer(filename);
+        rawAudioBuffer = stb_vorbis_decode_memory(soundFileBuffer, channelsBuffer, sampleRateBuffer);
+        //rawAudioBuffer = stb_vorbis_decode_filename("/tmp/test1.ogg", channelsBuffer, sampleRateBuffer);
+        System.out.println("Size " + rawAudioBuffer.limit() * Short.BYTES);
+
+        int channels = channelsBuffer.get(0);
+        int sampleRate = sampleRateBuffer.get(0);
+
+        int bufferPointer = alGenBuffers();
+        alBufferData(bufferPointer, channelsToFormat(channels), rawAudioBuffer, sampleRate);
+        return new SoundBuffer(bufferPointer);
     }
 
 }
